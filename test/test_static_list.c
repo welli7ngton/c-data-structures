7a7cba03b60e3a6c4621e4765bd6039916c0b2f1
Wellington - Trabalho <wellingtonasilva45@gmail.com>
2024-11-25 12:01:33 -0300

    test: fix bugs in test_remove_from_index and test_free_list

--- test/test_static_list.c
+++ test/test_static_list.c
@@ -29,13 +29,8 @@ void test_free_list() {
   free_list(test_list);
   Student test_student2 = {2, "default", 10.0, 8.9, 9.7};
   append_to_end(test_list, test_student2);
-  // ASSERT(test_list->qtd == 1 && test_list->data[1].register_number == 2,
-  //        "free_list() completely clean the list.");
-  // FIXME: this test isn't passing because of the function isint working
-  // properly
-  int *temp = &test_list->qtd;
-  printf("\nlist qtd: %d\n", *temp);
-  printf("\nreg_n: %d\n", test_list->data[0].register_number);
+  ASSERT(test_list->qtd == 1 && test_list->data[0].register_number == 2,
+         "free_list() completely clean the list.");
 }
 
 void test_list_length() {
@@ -159,7 +154,7 @@ void test_remove_from_index() {
 
   remove_from_index(test_list, 2);
 
-  ASSERT(/*test_list->data[1].register_number == 3 && */ test_list->qtd == 2,
+  ASSERT(test_list->data[1].register_number == 3 && test_list->qtd == 2,
          "remove_from_index() remove an item from its index.");
 }
 
@@ -168,7 +163,7 @@ int main() {
   printf("\n---------------------------------------------\n");
   test_create_list();
   test_appent_to_end();
-  // test_free_list();
+  test_free_list();
   test_list_length();
   test_is_full();
   test_is_not_full();
