a281e6b5ea0ac7a02eac4a2516cdc75feedd10b9
welli7ngton <wellingtonasilva45@gmail.com>
2024-11-26 19:02:25 -0300

    test: add a test for create_list()

--- test/dinamic_list.c
+++ test/dinamic_list.c
@@ -1,6 +1,15 @@
 #include "../src/dinamic_list/dinamic_list.h"
 #include "test_helpers.h"
+#include <stdio.h>
 
-void test_create_list() { List *li = create_list(); }
+void test_create_list() {
+  List *li = create_list();
+  ASSERT(*li == NULL,
+         "create_list() create a empty linked list, where the "
+         "first instance of an empty linked list is a NULL value.");
+}
 
-int main(int argc, char *argv[]) { return 0; }
+int main() {
+  test_create_list();
+  return 0;
+}
