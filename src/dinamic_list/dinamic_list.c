30aa02c15d3eec36e2049543435c91e506361e68
Wellington - Trabalho <wellingtonasilva45@gmail.com>
2024-11-27 14:41:25 -0300

    feat: add function is_full, is_empty, insert_to_start, insert_to_end, insert_sorting(bug)

--- src/dinamic_list/dinamic_list.c
+++ src/dinamic_list/dinamic_list.c
@@ -2,13 +2,6 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-struct element {
-  struct student data;
-  struct element *next;
-};
-
-typedef struct element Elem;
-
 List *create_list() {
   List *li = (List *)malloc(sizeof(List));
   if (li != NULL) {
@@ -27,4 +20,113 @@ void free_list(List *li) {
     }
     free(li);
   }
+}
+
+int list_length(List *li) {
+  if (li == NULL)
+    return -1;
+  int cont = 0;
+
+  Elem *node = *li;
+
+  while (node != NULL) {
+    cont++;
+    node = node->next;
+  }
+  return cont;
+}
+
+int is_empty(List *li) {
+  // list content
+  if (*li == NULL) {
+    return 1;
+  }
+  // list
+  if (li == NULL) {
+    return 1;
+  }
+  return 0;
+}
+
+int append_to_start(List *li, struct student st) {
+  if (li == NULL)
+    return 0;
+
+  Elem *new_node = (Elem *)malloc(sizeof(Elem));
+
+  if (new_node == NULL)
+    return 0;
+
+  new_node->data = st;
+  new_node->next = (*li);
+  *li = new_node;
+
+  return 1;
+}
+
+int append_to_end(List *li, struct student st) {
+  if (li == NULL)
+    return 0;
+
+  // verifiyng if the memory allocation was succeeded
+  Elem *new_node = (Elem *)malloc(sizeof(Elem));
+  if (new_node == NULL)
+    return 0;
+  new_node->data = st;
+  // as we know, this item will be the last item in the list, so it needs
+  // points to a null value, first we need to set new_node->next == NULL.
+  new_node->next = NULL;
+
+  if ((*li) == NULL) {
+    *li = new_node;
+  } else {
+    // saving the main head instance of the list
+    Elem *temp = *li;
+    // searching for the hole list for an NULL value, that means the end of the
+    // linked list.
+    while (temp->next != NULL) {
+      // step ahead
+      temp = temp->next;
+    }
+    // poiting the last value (NULL) to the new node.
+    temp->next = new_node;
+  }
+
+  return 1;
+}
+
+// FIXME: a função não está funcionando como esperado, bug linha 126.
+int append_sorting(List *li, struct student st) {
+  // sort by id ascendent count
+  if (li == NULL)
+    return 0;
+
+  Elem *new_node = (Elem *)malloc(sizeof(Elem));
+  if (new_node == NULL)
+    return 0;
+  new_node->data = st;
+  new_node->next = NULL;
+
+  if (is_empty(li)) {
+    new_node->next = (*li);
+    *li = new_node;
+    return 1;
+  } else {
+    Elem *_prev, *_curr = *li;
+
+    while (_curr != NULL && _curr->data.id < new_node->data.id) {
+      // step ahead
+      _prev = _curr;
+      _curr = _curr->next;
+    }
+    if (_curr == *li) {
+      new_node->next = (*li);
+      *li = new_node;
+    } else {
+      new_node->next = _curr;
+      _prev->next = new_node;
+    }
+  }
+
+  return 1;
 }
--- src/dinamic_list/dinamic_list.h
+++ src/dinamic_list/dinamic_list.h
@@ -6,6 +6,22 @@ struct student {
 
 typedef struct element *List;
 
+struct element {
+  struct student data;
+  struct element *next;
+};
+
+typedef struct element Elem;
 List *create_list();
 
 void free_list(List *li);
+
+int list_length(List *li);
+
+int is_empty(List *li);
+
+int append_to_start(List *li, struct student st);
+
+int append_to_end(List *li, struct student st);
+
+int append_sorting(List *li, struct student st);
