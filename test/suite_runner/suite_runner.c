35a6911738c210f906aefb83a99588719f5b2015
Wellington - Trabalho <wellingtonasilva45@gmail.com>
2024-12-04 11:32:04 -0300

    style: removing some unnecessary escape characters ('\n')

--- test/suite_runner/suite_runner.c
+++ test/suite_runner/suite_runner.c
@@ -4,13 +4,13 @@
 
 void run(func tests[], int amount_of_tests, char suite_name[]) {
 
-  printf("\nRunning tests for %s ...\n", suite_name);
+  printf("\nRunning tests for %s ...", suite_name);
   printf("\n---------------------------------------------\n");
 
-  printf("\n tests found: %d\n", amount_of_tests);
+  printf("\nTests found: %d\n", amount_of_tests);
   for (int i = 0; i < amount_of_tests; i++) {
     tests[i]();
   }
-  printf(GREEN "\nAll tests passed!\n" RESET);
+  printf(GREEN "\nAll tests passed!" RESET);
   printf("\n---------------------------------------------\n");
 }
