a363ce3fe2709ab9b608130925a441633a8dc6aa
Wellington - Trabalho <wellingtonasilva45@gmail.com>
2024-12-17 17:25:16 -0300

    feat: adiciona função que coloca itens no inicio da lista

--- src/doubly_linked_list/doubly_linked_list.c
+++ src/doubly_linked_list/doubly_linked_list.c
@@ -51,3 +51,26 @@ int is_empty(List *li) {
   }
   return 0;
 }
+
+
+int append_to_start(List *li, struct student st){
+  if (li == NULL) {
+    return -1;
+  }
+
+  Elem *node = (Elem*) malloc(sizeof(Elem));
+
+  if ((*li) == NULL){
+    return -1;
+  }
+
+  node->data = st;    // adicionando novo estudante.
+  node->next = (*li)  // colocando antigo inicio da lista como proximo item.
+  node->prev = NULL;  // como o node estará no inicio da lista ele deve apontar para nulo.
+
+  // caso o inicio da lista seja diferete de NULL (a lista nao está vazia).
+  if (*li != NULL){
+    (*li)->prev = node; // atual inicio da lista recebe o node como anterior a ele.
+  }
+  *li = node;  // inicio da lista recebe o node.
+}
